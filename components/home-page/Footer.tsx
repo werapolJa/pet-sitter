d4215cf3c86b153436e9b87b6cdab45d04d9a673
Tonkla <Apisit378819@gmail.com>
2024-11-26 14:55:25 +0700

    feat:add header and footer components

--- components/home-page/Footer.tsx
+++ components/home-page/Footer.tsx
@@ -1,8 +1,15 @@
+import logoWhite from "@/public/images/logoWhite.svg";
+import Image from "next/image";
+
 export default function Footer() {
-    return (
-      <div className="font-bold text-yellow-500">
-            Footer 
+  return (
+    <footer className="bg-black w-full flex flex-col justify-center items-center py-6">
+      <div>
+        <Image src={logoWhite} alt="Logo White" />
       </div>
-    );
-  }
-  
+      <h1 className="text-white text-lg mt-4">
+        Find your perfect pet sitter with us.
+      </h1>
+    </footer>
+  );
+}
