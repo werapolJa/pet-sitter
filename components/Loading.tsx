f288d5cdc8863c6052bf27dd1da8cd50dcd2c015
werapol <wesakaber@gmail.com>
2024-11-29 18:12:34 +0700

    feat:search in SearchBox and add searchchecklist

---
+++ components/Loading.tsx
@@ -0,0 +1,8 @@
+
+export default function LoadingPage() {
+  return (
+    <div className="max-w-3xl mx-auto h-auto flex items-center justify-center">
+      <span className="loading loading-spinner w-52 bg-gray-400"></span>
+    </div>
+  );
+}
