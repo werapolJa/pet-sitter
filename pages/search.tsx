11446a1d32d0b317935f17baa4ba77de3d9c97bb
werapol <wesakaber@gmail.com>
2024-12-04 10:22:06 +0700

    feat:redesign and onclick rating

--- pages/search.tsx
+++ pages/search.tsx
@@ -30,6 +30,8 @@ export default function SearchPage() {
   const [searchInput, setSearchInput] = useState<string>("");
   const [checkListArray, setCheckListArray] = useState<string[]>([]);
   const [textUrl, setTextUrl] = useState<string>("");
+  const [rating, setRating] = useState<number | null>(null);
+  console.log(rating);
 
   const CheckListInput = (value: string, checked: boolean) => {
     if (checked) {
@@ -97,209 +99,128 @@ export default function SearchPage() {
             <div className="flex flex-col md:flex-row gap-6 ">
               {/* left sidebar */}
 
-              <div className="w-full md:w-96 md:shadow-md md:rounded-xl h-full  md:sticky top-10">
-                <div className="space-y-6 m:pr-1 ">
-                  {/* input Search */}
+              <form action="" className="">
+                <div className="w-full md:w-96 md:shadow-md md:rounded-xl h-auto  md:sticky top-10">
+                  <div className="space-y-6 m:pr-1 ">
+                    {/* input Search */}
 
-                  <div className="md:px-5">
-                    <p className="py-5 hidden md:inline-block">Search</p>
+                    <div className="md:px-5">
+                      <p className="py-5 hidden md:inline-block">Search</p>
 
-                    <div className=" hidden md:flex w-full border border-gray-200 rounded-xl">
-                      <input
-                        type="text"
-                        className="py-2 rounded-lg outline-none border-0 w-full ml-2"
-                        onChange={(e) => SearchData(e.target.value)}
-                        value={searchInput}
-                      />
-                      <Image
-                        src={search}
-                        alt="search-icon"
-                        className="mr-2 ml-1 rounded-full"
-                        width={20}
-                        height={20}
-                        loading="lazy"
-                      />
+                      <div className=" hidden md:flex w-full border border-gray-200 rounded-xl">
+                        <input
+                          type="text"
+                          className="py-2 rounded-lg outline-none border-0 w-full ml-2"
+                          onChange={(e) => SearchData(e.target.value)}
+                          value={searchInput}
+                        />
+                        <Image
+                          src={search}
+                          alt="search-icon"
+                          className="mr-2 ml-1 rounded-full"
+                          width={20}
+                          height={20}
+                          loading="lazy"
+                        />
+                      </div>
                     </div>
-                  </div>
 
-                  {/* Pet Type */}
-                  <div className="space-y-2 bg-gray-100 w-full  px-5 py-6 md:px-5 md:bg-white">
-                    <h3 className="font-bold text-base">Pet Type:</h3>
-                    <div className="flex flex-wrap gap-2">
-                      {["Dog", "Cat", "Bird", "Rabbit"].map((pet) => (
-                        <label
-                          key={pet}
-                          className="flex items-center space-x-2"
+                    {/* Pet Type */}
+                    <div className="space-y-2 bg-gray-100 w-full  px-5 py-6 md:px-5 md:bg-white">
+                      <h3 className="font-bold text-base">Pet Type:</h3>
+                      <div className="flex flex-wrap gap-2">
+                        {["Dog", "Cat", "Bird", "Rabbit"].map((pet) => (
+                          <label
+                            key={pet}
+                            className="flex items-center space-x-2"
+                          >
+                            <input
+                              type="checkbox"
+                              className="checkbox font-semibold bg-white"
+                              value={pet}
+                              onClick={(e) =>
+                                CheckListInput(
+                                  (e.target as HTMLInputElement).value,
+                                  (e.target as HTMLInputElement).checked
+                                )
+                              }
+                            />
+                            <span>{pet}</span>
+                          </label>
+                        ))}
+                      </div>
+                    </div>
+
+                    {/* Rating */}
+                    <h3 className="font-bold text-base px-5 md:px-5">
+                      Rating:
+                    </h3>
+
+                    {/* Rating Start */}
+                    <div className="flex flex-wrap gap-2 px-5">
+                      {[
+                        // สร้าง array ที่มีค่าระดับการให้คะแนน (rating) และจำนวนดาว (stars) ที่ต้องการแสดง
+                        { rating: 5, stars: 5 },
+                        { rating: 4, stars: 4 },
+                        { rating: 3, stars: 3 },
+                        { rating: 2, stars: 2 },
+                        { rating: 1, stars: 1 },
+                      ].map(({ rating, stars }) => (
+                        // ใช้ .map เพื่อวนลูปแต่ละ object ใน array และสร้าง UI สำหรับแต่ละระดับคะแนน
+                        <div
+                          key={rating} // ใช้ rating เป็น key เพื่อระบุว่า element แต่ละตัวไม่ซ้ำกัน
+                          className="border-2 border-[#DCDFED] rounded-xl cursor-pointer hover:text-white hover:bg-green-500 group md:h-9 z-10"
+                          onClick={() => {
+                            setRating(rating);
+                          }}
+                          // กำหนดสไตล์ให้ div มีกรอบ (border), ขอบมน (rounded-xl) และเปลี่ยนสีพื้นหลังเมื่อ hover
                         >
-                          <input
-                            type="checkbox"
-                            className="checkbox font-semibold bg-white"
-                            value={pet}
-                            onClick={(e) =>
-                              CheckListInput(
-                                (e.target as HTMLInputElement).value,
-                                (e.target as HTMLInputElement).checked
-                              )
-                            }
-                          />
-                          <span>{pet}</span>
-                        </label>
+                          <div className="rating flex items-center justify-center px-2">
+                            {/* แสดงตัวเลขของระดับคะแนน */}
+                            <h1 className="text-xl mr-1 ">{rating}</h1>
+                            {/* วนลูปเพื่อสร้าง input (radio buttons) ที่แสดงดาวตามจำนวน stars */}
+                            {[...Array(stars)].map((_, index) => (
+                              <input
+                                key={index} // ใช้ index เป็น key สำหรับแต่ละดาว
+                                name={`rating-${rating}`} // ชื่อ group ของ radio แต่ละระดับคะแนน
+                                className="mask mask-star-2 bg-green-500 group-hover:bg-white "
+                                onClick={() => {
+                                  setRating(rating);
+                                }}
+                              />
+                            ))}
+                          </div>
+                        </div>
                       ))}
                     </div>
-                  </div>
 
-                  {/* Rating */}
-                  <h3 className="font-bold text-base px-5 md:px-5">Rating:</h3>
-                  <div className="flex flex-wrap gap-3 px-5 md:px-5">
-                    <div className="border-2 border-blue-5 inline px-2 py-3  rounded-xl cursor-pointer hover:text-white hover:bg-green-500  group">
-                      <div className="rating">
-                        <h1 className="text-xl mr-1">5</h1>
-                        <input
-                          type="radio"
-                          name="rating-1"
-                          className="mask mask-star-2 bg-green-500 group-hover:bg-white"
-                          disabled
-                        />
-                        <input
-                          type="radio"
-                          name="rating-1"
-                          className="mask mask-star-2 bg-green-500 group-hover:bg-white"
-                          disabled
-                        />
-                        <input
-                          type="radio"
-                          name="rating-1"
-                          className="mask mask-star-2 bg-green-500 group-hover:bg-white"
-                          disabled
-                        />
-                        <input
-                          type="radio"
-                          name="rating-1"
-                          className="mask mask-star-2 bg-green-500 group-hover:bg-white"
-                          disabled
-                        />
-                        <input
-                          type="radio"
-                          name="rating-1"
-                          className="mask mask-star-2 bg-green-500 group-hover:bg-white"
-                          disabled
-                        />
-                      </div>
+                    {/* Experience */}
+                    <div className="space-y-2 px-5 pt-3 md:px-5">
+                      <h3 className="font-bold pb-3">Experience:</h3>
+                      <select className="select select-bordered w-full hover:border-1 text-gray-400">
+                        <option>0-2 Years</option>
+                        <option>2-5 Years</option>
+                        <option>5+ Years</option>
+                      </select>
                     </div>
 
-                    <div className="border-2 border-blue-5 inline px-2 py-3 rounded-xl cursor-pointer hover:text-white hover:bg-green-500 group">
-                      <div className="rating">
-                        <h1 className="text-xl mr-1">4</h1>
-                        <input
-                          type="radio"
-                          name="rating-1"
-                          className="mask mask-star-2 bg-green-500 group-hover:bg-white"
-                          disabled
-                        />
-                        <input
-                          type="radio"
-                          name="rating-1"
-                          className="mask mask-star-2 bg-green-500 group-hover:bg-white"
-                          disabled
-                        />
-                        <input
-                          type="radio"
-                          name="rating-1"
-                          className="mask mask-star-2 bg-green-500 group-hover:bg-white"
-                          disabled
-                        />
-                        <input
-                          type="radio"
-                          name="rating-1"
-                          className="mask mask-star-2 bg-green-500 group-hover:bg-white"
-                          disabled
-                          defaultChecked
-                        />
-                      </div>
+                    {/* Buttons Search*/}
+                    <div className="inline-block w-full  gap-2 px-5 md:hidden ">
+                      <button className="btn bg-orange-500 hover:bg-white hover:text-orange-500 hover:border-orange-500 rounded-full w-full text-white btn-lg ">
+                        Search
+                      </button>
                     </div>
-
-                    <div className="border-2 border-blue-5 inline px-2 py-3 rounded-xl cursor-pointer hover:text-white hover:bg-green-500 group">
-                      <div className="rating">
-                        <h1 className="text-xl mr-1">3</h1>
-                        <input
-                          type="radio"
-                          name="rating-1"
-                          className="mask mask-star-2 bg-green-500 group-hover:bg-white"
-                          disabled
-                        />
-                        <input
-                          type="radio"
-                          name="rating-1"
-                          className="mask mask-star-2 bg-green-500 group-hover:bg-white"
-                          disabled
-                        />
-                        <input
-                          type="radio"
-                          name="rating-1"
-                          className="mask mask-star-2 bg-green-500 group-hover:bg-white"
-                          disabled
-                          defaultChecked
-                        />
-                      </div>
+                    <div className="hidden md:flex gap-2 px-5 py-6 md:px-5">
+                      <button className="btn flex-1 bg-orange-100 text-orange-500 rounded-full">
+                        Clear
+                      </button>
+                      <button className="btn flex-1 bg-orange-500 text-white rounded-full">
+                        Search
+                      </button>
                     </div>
-
-                    <div className="border-2 border-blue-5 inline px-2 py-3  rounded-xl cursor-pointer hover:text-white hover:bg-green-500 group">
-                      <div className="rating">
-                        <h1 className="text-xl mr-1">2</h1>
-                        <input
-                          type="radio"
-                          name="rating-1"
-                          className="mask mask-star-2 bg-green-500 group-hover:bg-white"
-                          disabled
-                        />
-                        <input
-                          type="radio"
-                          name="rating-1"
-                          className="mask mask-star-2 bg-green-500 group-hover:bg-white"
-                          disabled
-                        />
-                      </div>
-                    </div>
-
-                    <div className="border-2 border-blue-5 inline px-2 py-3  rounded-xl cursor-pointer hover:text-white hover:bg-green-500  group">
-                      <div className="rating">
-                        <h1 className="text-xl mr-1">1</h1>
-                        <input
-                          type="radio"
-                          name="rating-1"
-                          className="mask mask-star-2 bg-green-500 group-hover:bg-white"
-                          disabled
-                        />
-                      </div>
-                    </div>
-                  </div>
-
-                  {/* Experience */}
-                  <div className="space-y-2 px-5 pt-3 md:px-5">
-                    <h3 className="font-bold pb-3">Experience:</h3>
-                    <select className="select select-bordered w-full hover:border-1 text-gray-400">
-                      <option>0-2 Years</option>
-                      <option>2-5 Years</option>
-                      <option>5+ Years</option>
-                    </select>
-                  </div>
-
-                  {/* Buttons Search*/}
-                  <div className="inline-block w-full  gap-2 px-5 md:hidden">
-                    <button className="btn bg-orange-500 hover:bg-white hover:text-orange-500 hover:border-orange-500 rounded-full w-full text-white btn-lg ">
-                      Search
-                    </button>
-                  </div>
-                  <div className="hidden md:flex gap-2 px-5 py-6 md:px-5">
-                    <button className="btn flex-1 bg-orange-100 text-orange-500 rounded-full">
-                      Clear
-                    </button>
-                    <button className="btn flex-1 bg-orange-500 text-white rounded-full">
-                      Search
-                    </button>
                   </div>
                 </div>
-              </div>
+              </form>
 
               {/* Search For Pet Sitter moblie */}
               <div className="container mx-auto flex flex-col items-center md:hidden">
@@ -327,8 +248,8 @@ export default function SearchPage() {
                             <Image
                               src={sitter.image_1}
                               alt={sitter.full_name}
-                              width={500}
-                              height={300}
+                              width={200}
+                              height={20}
                               className="w-[90%] h-48 lg:w-96  object-cover ml-5 rounded-xl"
                             />
                           ) : (
@@ -337,6 +258,8 @@ export default function SearchPage() {
                                 src={imagebgicon}
                                 alt={sitter.full_name}
                                 className="object-cover rounded-xl lg:ml-3 md:w-96"
+                                width={20}
+                                height={20}
                               />
                             </div>
                           )}
@@ -360,6 +283,8 @@ export default function SearchPage() {
                                         src={profiledefault}
                                         alt="search-icon"
                                         className=" rounded-full"
+                                        width={20}
+                                        height={20}
                                       />
                                     </div>
                                   )}
@@ -392,7 +317,7 @@ export default function SearchPage() {
                             </div>
 
                             <div className="flex items-center text-sm gap-2  xl:py-4">
-                              <Image src={mark} alt="" />
+                              <Image src={mark} alt="" width={20} height={20} />
                               <h1 className="text-gray-400 py-4 md:py-4">
                                 {sitter.place}
                               </h1>
