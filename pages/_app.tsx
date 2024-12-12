ed3c93b3e4d3f819770eb63a14b09ff8e81d0f76
Bunyawat Singkiporn <pupahero@gmail.com>
2024-12-12 12:57:01 +0700

    fix: login admin error 401 by removing JWT interceptor

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,9 +1,7 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
-import jwtInterceptor from "@/utils/jwtInterceptor";
 import { AuthProvider } from "@/context/authentication";
-import {SearchProvider} from "@/context/searchbar"
-jwtInterceptor();
+import { SearchProvider } from "@/context/searchbar";
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
