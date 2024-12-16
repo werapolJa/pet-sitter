e47531c8a2ffc19610540016ad11e1864fe7204b
Bunyawat Singkiporn <pupahero@gmail.com>
2024-12-16 19:03:31 +0700

    style: adjust rating and overall style in review part for admin petowner page

--- pages/admin/petowners/profile.tsx
+++ pages/admin/petowners/profile.tsx
@@ -236,31 +236,57 @@ const AdminPetownerProfile = () => {
 
       case "reviews":
         return (
-          <div className="p-6 bg-white rounded-b-lg rounded-tr-lg">
-            <h2 className="text-lg font-bold mb-4">Reviews</h2>
+          <div className="p-6 bg-white rounded-b-lg rounded-tr-lg pt-10">
             {userData.reviews && userData.reviews.length > 0 ? (
               userData.reviews.map((review: Review, index: number) => (
-                <div key={index} className="mb-6 border-b pb-4">
-                  <div className="flex items-center mb-2">
-                    <Image
-                      src={
-                        review.petsitter_image ||
-                        "https://via.placeholder.com/50"
-                      }
-                      alt="{review.petsitter_name}"
-                      width={50}
-                      height={50}
-                      className="rounded-full mr-4"
-                    />
+                <div key={index} className="mb-6 border-b pb-4 flex gap-10">
+                  <div className="flex items-center mb-2 ml-8 gap-3">
+                    <div className="w-14 h-14 rounded-full overflow-hidden flex-shrink-0 relative">
+                      <Image
+                        src={
+                          review.petsitter_image ||
+                          "https://via.placeholder.com/104"
+                        }
+                        alt="Pet"
+                        layout="fill"
+                        objectFit="cover"
+                        objectPosition="center"
+                      />
+                    </div>
                     <div>
-                      <p className="font-semibold">{review.petsitter_name}</p>
+                      <p className="w-36 text-lg font-medium">
+                        {review.petsitter_name}
+                      </p>
                       <p className="text-sm text-gray-500">
-                        {new Date(review.review_date).toLocaleDateString()}
+                        {new Date(review.review_date).toLocaleDateString(
+                          "en-US",
+                          {
+                            month: "short",
+                            day: "2-digit",
+                            year: "numeric",
+                          }
+                        )}
                       </p>
                     </div>
                   </div>
-                  <p className="mb-2">Rating: {review.rating} / 5</p>
-                  <p>{review.review_message}</p>
+                  <div className="flex flex-col gap-4">
+                    <div className="rating">
+                      {Array.from({ length: 5 }, (_, i) => (
+                        <input
+                          key={i}
+                          type="radio"
+                          name={`rating-${index}`} // Unique name for each review
+                          className="mask mask-star-2 bg-green-500 w-5 h-5"
+                          checked={i + 1 == review.rating} // Dynamically control which star is checked
+                          readOnly // Make it read-only since it's just for display
+                        />
+                      ))}
+                    </div>
+
+                    <p className="text-base text-gray-500">
+                      {review.review_message}
+                    </p>
+                  </div>
                 </div>
               ))
             ) : (
